e1178b26e44636b45fa9c9742260117d9bebc9d9
KingsleyDeng <460556933@qq.com>
2020-03-16 18:28:01 +0800

    Flask 渲染模板

--- Flask变量规则.py
+++ Flask变量规则.py
@@ -3,38 +3,29 @@
 from flask import Flask
 from flask import url_for
 from flask import redirect
+from flask import render_template
 
 app = Flask(__name__)
 
-
-@app.route("/index")
-def index():
-    return "Hello Index"
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 
 @app.route("/")
-def hello():
-    return "Hello Worlds!"
+def index():
+    return render_template('index.html',name=name, movies=movies)
 
-
-@app.route('/user/<username>')
-def show_user_profile(username): \
-        # show the user profile for that user
-    return 'User %s' % username
-
-
-# int 接受整数
-# float 接受浮点数
-# path 和缺省情况一样，但也可以接受斜杠
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return 'Post %d' % post_id
-
-
-@app.route('/test')
-def test_url_for():
-    url = url_for("hello")
-    return redirect(url)
 
 
 
