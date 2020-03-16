27d3befb2fdb0b34139928e8ea1810db4f3bc220
KingsleyDeng <460556933@qq.com>
2020-03-16 17:06:01 +0800

    Flask 重定向

--- Flask变量规则.py
+++ Flask变量规则.py
@@ -1,6 +1,8 @@
 # -- coding: UTF-8 --
 
 from flask import Flask
+from flask import url_for
+from flask import redirect
 
 app = Flask(__name__)
 
@@ -10,9 +12,9 @@ def index():
     return "Hello Index"
 
 
-@app.route("/hello")
+@app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello Worlds!"
 
 
 @app.route('/user/<username>')
@@ -20,12 +22,20 @@ def show_user_profile(username): \
         # show the user profile for that user
     return 'User %s' % username
 
+
 # int 接受整数
 # float 接受浮点数
 # path 和缺省情况一样，但也可以接受斜杠
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     return 'Post %d' % post_id
+
+
+@app.route('/test')
+def test_url_for():
+    url = url_for("hello")
+    return redirect(url)
+
 
 
 if __name__ == "__main__":
--- HelloFlask.py
+++ HelloFlask.py
@@ -12,5 +12,4 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.debug = True
     app.run()
