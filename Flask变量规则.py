262b1a6f52d3d2d8e045fa4936c377b89833c520
KingsleyDeng <460556933@qq.com>
2020-03-17 15:21:10 +0800

    添加静态文件

--- Flask变量规则.py
+++ Flask变量规则.py
@@ -24,9 +24,7 @@ movies = [
 
 @app.route("/")
 def index():
-    return render_template('index.html',name=name, movies=movies)
-
-
+    return render_template('index.html', name=name, movies=movies)
 
 
 if __name__ == "__main__":
