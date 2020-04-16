e5d35173636334500820d100e11c7e73a167dab4
KingsleyDeng <460556933@qq.com>
2020-04-16 17:19:30 +0800

    Flask 蓝图 使用方式

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from flask import Flask
 from goods import get_goods
 from users import register
 from orders import app_orders
+from cart import app_cart
+
 # 循环引用 解决方法 推迟一方的导入 让另一方先完成
 
 
@@ -14,12 +16,15 @@ app.route("/register")(register)
 
 # 注册蓝图
 # app.register_blueprint(app_orders)
-app.register_blueprint(app_orders,url_prefix="/orders")
+app.register_blueprint(app_orders, url_prefix="/orders")
+app.register_blueprint(app_cart, url_prefix="/cart")
+
 
 @app.route("/")
 def index():
     return "index page"
 
+
 if __name__ == '__main__':
     print(app.url_map)
-    app.run()
+    app.run(debug=True)
